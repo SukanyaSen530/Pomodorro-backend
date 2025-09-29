24226a7d9987e2b6f7679ca796b51b4e1c341987
Sukanya Sen <vdr_sen.sukanya@airasia.com>
2025-09-29 15:35:57 +0530

    fix - vercel deployment issue

--- index.js
+++ index.js
@@ -21,11 +21,9 @@ app.use(cors());
 app.use(express.json({ limit: "5mb" }));
 app.use(express.urlencoded({ extended: false }));
 
-
 app.get("/", (req, res) => res.send("Pomodoro Backend!"));
 app.use("/auth", authRoutes);
 app.use("/tasks", protectedRoutes, taskRoutes);
-
 
 const PORT = process.env.PORT || 5000;
 
@@ -35,4 +33,6 @@ const server = app.listen(PORT, (err) => {
   console.log(`Server running on ${port}`);
 });
 
-export default app;
+export default async function handler(req, res) {
+  return app(req, res);
+}
