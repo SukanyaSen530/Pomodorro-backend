00070ce199bb1f73046f678a3c5cff445d7d0947
Sukanya Sen <vdr_sen.sukanya@airasia.com>
2025-09-30 23:30:05 +0530

    fix - vercel.json

--- index.js
+++ index.js
@@ -17,7 +17,12 @@ const app = express();
 //connecting to mongoDB
 connectDB();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: [process.env.FRONTEND_URL, "http://localhost:3000"],
+    credentials: true,
+  })
+);
 app.use(express.json({ limit: "5mb" }));
 app.use(express.urlencoded({ extended: false }));
 
